e4c5d3e9d2f518be68c1e3c69060793a60f69b4e
Cayke Daniel <contato.cayke@gmail.com>
2025-08-26 21:49:56 -0300

    fix: adicionada a remoção ao card de produto

--- app/router/routes.php
+++ app/router/routes.php
@@ -5,5 +5,5 @@ $routes = [
     "/add-product" => "ProductController@create",
     "/edit-product/{id}" => "ProductController@edit",
     "/update-product/{id}" => "ProductController@update",
-    
+    "/delete-product/{id}" => "ProductController@delete",
 ];
--- app/views/partials/cards.php
+++ app/views/partials/cards.php
@@ -31,7 +31,7 @@
             <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
             </svg>
           </div>Detalhes</a>
-          <a href="#" class="btn btn--danger" aria-label="Remover Camiseta Essential">
+          <a href="/delete-product/<?= $item['id'] ?>" class="btn btn--danger" aria-label="Remover Camiseta Essential">
             <div class="icon remove-icon">
             <svg viewBox="0 0 24 24">
             <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
